f3d7eacc135c56bbd1c05f304452a2afc66c20cf
Michael Abbott <improbable@gmail.com>
2019-04-08 05:01:19 -0400

    fix JuliennedArrays

--- src/julienne.jl
+++ src/julienne.jl
@@ -1,28 +1,35 @@
 
 using .JuliennedArrays
 
-# import TensorCast: julienne_slice, julienne_glue # because Revise doesn't track these
+# import TensorCast: julienne_slice, julienne_glue, newcode, jcollect # because Revise doesn't track these
 
 if isdefined(JuliennedArrays, :julienne) # old notation
 
     @inline julienne_slice(A::AbstractArray, code::Tuple) where {T,N} =
-        collect(julienne(A, code)) 
+        collect(julienne(A, code))
 
     @inline julienne_glue(A::AbstractArray{IT,N}, code::Tuple) where {IT,N} =
         align(A, code)
 
 elseif isdefined(JuliennedArrays, :Slices) # new notation
 
-    @warn "TensorCast only partially works with the new version of JuliennedArrays, sorry."
-
     @inline julienne_slice(A::AbstractArray{T,N}, code::Tuple) where {T,N} =
         Slices(A, newcode(code)...)
 
     @inline julienne_glue(A::AbstractArray{IT,N}, code::Tuple) where {IT,N} =
-        collect(Align(A, newcode(code)...))
+        jcollect(Align(A, newcode(code)...))
 
     @generated newcode(code::Tuple) = Expr(:tuple, [ c == Colon ? True() : False() for c in code.parameters ]...)
 
+    # collect(Align(... and copy(Align(... both give errors
+    function jcollect(A::JuliennedArrays.Align{T,N}) where {T,N}
+        out = Array{T}(undef, size(A))
+        @inbounds for I in CartesianIndices(A)
+            out[I] = A[I.I...]
+        end
+        out
+    end
+
 else
-    @warn "@shape can't seem to find functions from JuliennedArrays. It should fall back to base methods in most cases."
+    @warn "TensorCast failed to load functions from JuliennedArrays"
 end
