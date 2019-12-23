5568230aa8892626b31ff39629554e3608eabd5d
Michael Abbott <me@escbook>
2019-12-23 16:43:00 +0100

    using Compat.jl

--- src/TensorCast.jl
+++ src/TensorCast.jl
@@ -3,7 +3,7 @@ module TensorCast
 
 export @cast, @reduce, @matmul, @pretty
 
-using MacroTools, StaticArrays
+using MacroTools, StaticArrays, Compat
 using LinearAlgebra, Random
 
 include("macro.jl")
@@ -15,8 +15,6 @@ include("view.jl")      # orient, Reverse{d} etc
 include("recursive.jl") # RecursiveArrayTools
 include("lazy.jl")      # LazyCast
 include("static.jl")    # StaticArrays
-
-@static VERSION < v"1.1.0" && include("eachslice.jl")
 
 include("warm.jl")
 
